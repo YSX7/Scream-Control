fb53c24f835e27a8e045b226cbd5e705b0ab600a
YSX <ysx080@gmail.com>
2017-05-29 21:18:03 +0500

    Added mvvm test

---
+++ MVVM_Test/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MVVM_Test.ViewModel;
+using MVVM_Test.View;
+using System.Windows;
+
+namespace MVVM_Test
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var mw = new MainWindow
+            {
+                DataContext = new MainWindowViewModel()
+            };
+             mw.Show();
+        }
+
+    }
+}
---
+++ MVVM_Test/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MVVM_Test.Model;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MVVM_Test.ViewModel
+{
+    class MainWindowViewModel
+    {
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            ClickCommand = new Command(arg => ClickMethod());
+            People = new PeopleModel
+            {
+                FirstName = "First name",
+                LastName = "Last name"
+            };
+        }
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Get or set people.
+        /// </summary>
+        public PeopleModel People { get; set; }
+
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// Gets or sets ClickCommand
+        /// </summary>
+        public ICommand ClickCommand { get; set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Click method
+        /// </summary>
+        private void ClickMethod()
+        {
+            MessageBox.Show("Hello mister fuzkoff");
+        }
+        #endregion
+    }
+}
